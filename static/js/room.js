7319cb45b1d8e423ad6b82174ea144266d5cf841
ArnavPundir22 <arnavp128@gmail.com>
2026-06-18 20:34:33 +0530

    fix: add visual feedback when copying invite link

--- static/js/room.js
+++ static/js/room.js
@@ -848,8 +848,8 @@ if (inviteBtn) {
             const originalIcon = inviteBtn.innerHTML;
             inviteBtn.innerHTML = `
             <svg xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
+                width="16"
+                height="16"
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
@@ -858,8 +858,13 @@ if (inviteBtn) {
                 stroke-linejoin="round">
                 <path d="M20 6 9 17l-5-5"></path>
             </svg>
-            `;
+            Copied!`;
             inviteBtn.classList.add('active');
+
+            // Add system message to chat
+            if (typeof addChatMessage === 'function') {
+                addChatMessage('System', 'Invite link copied to clipboard!', true);
+            }
 
             setTimeout(() => {
                 inviteBtn.innerHTML = originalIcon;
