d68ca8f3a98c56ea98dd91219743efa7d22e5f28
ArnavPundir22 <arnavp128@gmail.com>
2026-07-04 11:19:54 +0530

    chore: add Google Search Console verification file and route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,31 @@
-const express = require('express');
-const path = require('path');
-const roomController = require('../controllers/room.controller');
+const express = require("express");
+const path = require("path");
+const roomController = require("../controllers/room.controller");
 
 const router = express.Router();
 
 // Static routes
-router.get('/sw.js', (req, res) => res.sendFile(path.join(__dirname, '../../static/sw.js')));
-router.get('/manifest.json', (req, res) => res.sendFile(path.join(__dirname, '../../static/manifest.json')));
-router.get('/favicon.ico', (req, res) => res.status(404).send());
-router.get('/about', (req, res) => res.render('about'));
+router.get("/sw.js", (req, res) =>
+  res.sendFile(path.join(__dirname, "../../static/sw.js")),
+);
+router.get("/manifest.json", (req, res) =>
+  res.sendFile(path.join(__dirname, "../../static/manifest.json")),
+);
+router.get("/google6ebffc7c3f9362cb.html", (req, res) =>
+  res.sendFile(
+    path.join(__dirname, "../../static/google6ebffc7c3f9362cb.html"),
+  ),
+);
+router.get("/favicon.ico", (req, res) => res.status(404).send());
+router.get("/about", (req, res) => res.render("about"));
 
 // Room routes
-router.get('/', roomController.renderIndex);
-router.post('/', roomController.handleAction);
-router.get('/join/:room_code', roomController.joinDirect);
-router.get('/secure/env-:encoded_room/session/sess-:encoded_user', roomController.renderRoom);
+router.get("/", roomController.renderIndex);
+router.post("/", roomController.handleAction);
+router.get("/join/:room_code", roomController.joinDirect);
+router.get(
+  "/secure/env-:encoded_room/session/sess-:encoded_user",
+  roomController.renderRoom,
+);
 
 module.exports = router;
